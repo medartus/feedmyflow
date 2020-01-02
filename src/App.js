9186436a0d5ecc645f315118c065f38e87f04dca
MarcEtienneDartus <marcetienne.dartus@gmail.com>
2020-01-02 15:09:53 +0100

    Update with hooks Menu.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Menu from './view/menu/menu';
 // import Login from './view/login/login';
 import './App.css';
 
-class App extends Component {
-  render(){
-    return (
-      <Menu/>
-    );
-  }
+export const App = () => {
+  return (
+    <Menu/>
+  );
 }
 
 export default App;
--- src/view/menu/menu.js
+++ src/view/menu/menu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import {Grid,TextField,Container,Select,MenuItem,Toolbar,AppBar,Typography,Card,CardContent,Divider,Button} from '@material-ui/core';
 import {ExpansionPanel,ExpansionPanelDetails,ExpansionPanelSummary} from '@material-ui/core';
@@ -9,83 +9,43 @@ import fire from '../../provider/firebase'
 import './menu.css';
 
 
-class Menu extends Component {
-    constructor(props){
-        super(props)
-        this.cookies = new Cookies();
-        this.state = {
-            publicationTime:new Date(),
-            shareCommentary:'',
-            shareMediaCategory:'NONE',
-            visibility:'PUBLIC',
-            mediaTitle:'',
-            mediaDescription:'',
-            mediaUrl:'',
-            expanded : '',
-            setExpanded : false,
-            items: [],
-        }
-        this.currentUser = undefined;
-        this.userUid = undefined;
-        this.db = fire.firestore();
-    }
+const Menu = () => {
+
     
-    handleChange = panel => (event, isExpanded) => {
-        this.setState({expanded:isExpanded ? panel : false});
+    const [publicationTime,setPublicationTime] = useState(new Date());
+    const [shareCommentary,setShareCommentary] = useState('');
+    const [shareMediaCategory,setShareMediaCategory] = useState('NONE');
+    const [visibility,setVisibility] = useState('PUBLIC');
+    const [mediaTitle,setMediaTitle] = useState('');
+    const [mediaDescription,setMediaDescription] = useState('');
+    const [mediaUrl,setMediaUrl] = useState('');
+    const [expanded,setExpanded] = useState('');
+    const [items,setItems] = useState([]);
+
+    let cookies = new Cookies();
+    let currentUser = undefined;
+    let userUid = undefined;
+    let db = fire.firestore();
+    
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false)
     };
 
-    componentDidMount(){
-        let token = undefined;
-        try {
-            token = this.cookies.get('token');
-        } catch (error) {console.log(error)} 
-        if(token !== undefined){
-            this.firebaseInit(token);
-        }
-        else{
-            this.getToken();
-        }
-    }
-
-    getURLParameter(name) {
+    const getURLParameter = (name) => {
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(window.location.href) ||
             [null, ''])[1].replace(/\+/g, '%20')) || null;
     }
 
-    async getToken(){
-        var code = this.getURLParameter('code');
-        var state = this.getURLParameter('state');
-        // var error = this.getURLParameter('error');
-        if(code!==null){
-            // This is the URL to the HTTP triggered 'token' Firebase Function.
-            var tokenFunctionURL = 'https://us-central1-feedmyflow.cloudfunctions.net/token';
-            let url = tokenFunctionURL +
-                '?code=' + encodeURIComponent(code) +
-                '&state=' + encodeURIComponent(state);
-            let res = await this.get(url);
-            if (res.token) {
-                this.cookies.set('token', res.token.toString());
-                this.firebaseInit(res.token)
-            } else {
-                document.body.innerText = 'Error in the token Function: ' + res.error;
-            }
-        }
-    }
-    
-    firebaseInit(token){
+    const firebaseInit = (token) => {
         fire.auth().signInWithCustomToken(token).then(() => {
-            this.currentUser = fire.auth().currentUser;
-            this.userUid = this.currentUser.uid;
-            this.db.collection('user').doc(this.userUid).collection('post').onSnapshot((documents)=>{this.setState({items:documents.docs})});
+            currentUser = fire.auth().currentUser;
+            userUid = currentUser.uid;
+            db.collection('user').doc(userUid).collection('post').onSnapshot((documents)=>{setItems(documents.docs)});
         })
-        .catch(()=>{
-            console.log("auth Error")
-            this.getToken()
-        });
-        // .catch(()=>{this.getToken()});
+        .catch(()=>{getToken()});
     }
-    
-    async get(url){
+
+    const get = async (url) => {
         try {
             let response = await fetch(url, {method: "GET"});
             const jsondata = await response.json();
@@ -95,16 +55,36 @@ class Menu extends Component {
             throw error;
         }    
     }
+
+    const getToken = async () => {
+        var code = getURLParameter('code');
+        var state = getURLParameter('state');
+        // var error = this.getURLParameter('error');
+        if(code!==null){
+            // This is the URL to the HTTP triggered 'token' Firebase Function.
+            var tokenFunctionURL = 'https://us-central1-feedmyflow.cloudfunctions.net/token';
+            let url = tokenFunctionURL +
+                '?code=' + encodeURIComponent(code) +
+                '&state=' + encodeURIComponent(state);
+            console.log('plop')
+            console.log(code)
+            console.log(state)
+            let res = await get(url);
+            if (res.token) {
+                cookies.set('token', res.token.toString());
+                firebaseInit(res.token)
+            } else {
+                document.body.innerText = 'Error in the token Function: ' + res.error;
+            }
+        }
+    }
+    
     
 
-    onSendData = () => {
-        // let tempTime = this.publicationTime.split(":")
-        // this.date.setHours(parseInt(tempTime[0]))
-        // this.date.setMinutes(parseInt(tempTime[1]))
-        const {publicationTime,shareCommentary,visibility,shareMediaCategory,mediaTitle,mediaDescription,mediaUrl} = this.state;
+    const onSendData = () => {
         let linekdinPost = {
-            "author":"urn:li:person:"+this.userUid.split(':')[1],
-            "userUID":this.userUid,
+            "author":"urn:li:person:"+userUid.split(':')[1],
+            "userUID":userUid,
             "publicationTime":publicationTime,
             "shareCommentary": shareCommentary,
             "visibility": visibility,
@@ -119,19 +99,18 @@ class Menu extends Component {
                 "originalUrl":mediaUrl,
             }
         }
-        this.db.collection('user').doc(this.userUid).collection('post').add(linekdinPost)
+        db.collection('user').doc(userUid).collection('post').add(linekdinPost)
     }
 
-    onLogin(){
-        window.location.href = "https://us-central1-feedmyflow.cloudfunctions.net/redirect";
+    // const onLogin = () => {
+    //     window.location.href = "https://us-central1-feedmyflow.cloudfunctions.net/redirect";
+    // }
+
+    const deletePost = (postId) => {
+        db.collection('user').doc(this.userUid).collection('post').doc(postId).delete()
     }
 
-    deletePost = (postId) => {
-        this.db.collection('user').doc(this.userUid).collection('post').doc(postId).delete()
-    }
-
-    mediaRender = () =>{
-        const { shareMediaCategory, mediaTitle, mediaDescription, mediaUrl } = this.state
+    const mediaRender = () => {
         if (shareMediaCategory !== 'NONE'){
             return(
                 <Grid container spacing={3}>
@@ -141,7 +120,7 @@ class Menu extends Component {
                             id="mediaTitle"
                             label="Titre"
                             value={mediaTitle}
-                            onChange={e=>this.setState({mediaTitle:e.target.value})}
+                            onChange={e=>setMediaTitle(e.target.value)}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -150,7 +129,7 @@ class Menu extends Component {
                             id="mediaDescription"
                             label="Description"
                             value={mediaDescription}
-                            onChange={e=>this.setState({mediaDescription:e.target.value})}
+                            onChange={e=>setMediaDescription(e.target.value)}
                             multiline
                             />
                     </Grid>
@@ -160,7 +139,7 @@ class Menu extends Component {
                             id="mediaUrl"
                             label="Url"
                             value={mediaUrl}
-                            onChange={e=>this.setState({mediaUrl:e.target.value})}
+                            onChange={e=>setMediaUrl(e.target.value)}
                         />
                     </Grid>
                 </Grid>
@@ -168,12 +147,11 @@ class Menu extends Component {
         }
     }
 
-    postRender = () =>{
-        const { items, expanded } = this.state;
+    const postRender = () =>{
         return(
             <>
                 {items.map(data => (
-                    <ExpansionPanel expanded={expanded === data.id} onChange={this.handleChange(data.id)} key={data.id}>
+                    <ExpansionPanel expanded={expanded === data.id} onChange={handleChange(data.id)} key={data.id}>
                         <ExpansionPanelSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1bh-content"
@@ -192,7 +170,7 @@ class Menu extends Component {
                             </div>
                             }
                             
-                            <Button variant="contained" color="secondary" onClick={()=>{this.deletePost(data.id)}}>
+                            <Button variant="contained" color="secondary" onClick={()=>{deletePost(data.id)}}>
                                 Delete
                             </Button>
                         </ExpansionPanelDetails>
@@ -202,93 +180,104 @@ class Menu extends Component {
         )
     }
 
-    render(){
-        const { shareCommentary, publicationTime, shareMediaCategory, visibility } = this.state;
-        return (
-            <div>
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                        <Typography className="title" variant="h6">FeedMyFlow</Typography>
-                    </Toolbar>
-                </AppBar>
-                <Container className="menu">
-                    <Card>
-                        <CardContent>
-                            <Typography variant="h5" component="h2">Create</Typography>
-                            <Divider/>
-                            <Grid container spacing={3}>
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <Grid item xs={12} md={2}>
-                                        <KeyboardDatePicker
-                                            margin="normal"
-                                            id="date-picker-dialog"
-                                            label="Date picker dialog"
-                                            format="dd/MM/yyyy"
-                                            value={publicationTime}
-                                            onChange={e=>this.setState({publicationTime:e})}
-                                            KeyboardButtonProps={{
-                                                'aria-label': 'change date',
-                                            }}
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12} md={2}>
-                                        <KeyboardTimePicker
-                                            margin="normal"
-                                            id="time-picker"
-                                            label="Time picker"
-                                            value={publicationTime}
-                                            onChange={e=>this.setState({publicationTime:e})}
-                                            KeyboardButtonProps={{
-                                                'aria-label': 'change time',
-                                            }}
-                                        />
-                                    </Grid>
-                                </MuiPickersUtilsProvider>
+    
+    useEffect(()=>{
+        let token = undefined;
+        try {
+            token = cookies.get('token');
+        } catch (error) {console.log(error)} 
+        if(token !== undefined){
+            firebaseInit(token);
+        }
+        else{
+            getToken();
+        }
+    },[])
+
+    return (
+        <div>
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <Typography className="title" variant="h6">FeedMyFlow</Typography>
+                </Toolbar>
+            </AppBar>
+            <Container className="menu">
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">Create</Typography>
+                        <Divider/>
+                        <Grid container spacing={3}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
                                 <Grid item xs={12} md={2}>
-                                    <Select
-                                        labelId="Type de post"
-                                        id="shareMediaCategory"
-                                        value={shareMediaCategory}
-                                        onChange={e=>this.setState({shareMediaCategory:e.target.value})}>
-                                            <MenuItem value={'NONE'}>Juste Text</MenuItem>
-                                            <MenuItem value={'ARTICLE'}>Avec Article</MenuItem>
-                                            <MenuItem value={'IMAGE'}>Avec Image</MenuItem>
-                                    </Select>
-                                </Grid>
-                                <Grid item xs={12} md={2}>
-                                    <Select
-                                        labelId="Visibilité"
-                                        id="visibility"
-                                        value={visibility}
-                                        onChange={e=>this.setState({visibility:e.target.value})}>
-                                            <MenuItem value={'PUBLIC'}>Publique</MenuItem>
-                                            <MenuItem value={'CONNECTIONS'}>connexions</MenuItem>
-                                    </Select>
-                                </Grid>
-                            </Grid>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12}>
-                                    <TextField 
-                                        fullWidth 
-                                        label="Contenu du post" 
-                                        id="shareCommentary"
-                                        value={shareCommentary}
-                                        onChange={e=>this.setState({shareCommentary:e.target.value})}
-                                        multiline
+                                    <KeyboardDatePicker
+                                        margin="normal"
+                                        id="date-picker-dialog"
+                                        label="Date picker dialog"
+                                        format="dd/MM/yyyy"
+                                        value={publicationTime}
+                                        onChange={e=>setPublicationTime(e)}
+                                        KeyboardButtonProps={{
+                                            'aria-label': 'change date',
+                                        }}
                                     />
                                 </Grid>
+                                <Grid item xs={12} md={2}>
+                                    <KeyboardTimePicker
+                                        margin="normal"
+                                        id="time-picker"
+                                        label="Time picker"
+                                        value={publicationTime}
+                                        onChange={e=>setPublicationTime(e)}
+                                        KeyboardButtonProps={{
+                                            'aria-label': 'change time',
+                                        }}
+                                    />
+                                </Grid>
+                            </MuiPickersUtilsProvider>
+                            <Grid item xs={12} md={2}>
+                                <Select
+                                    labelId="Type de post"
+                                    id="shareMediaCategory"
+                                    value={shareMediaCategory}
+                                    onChange={e=>setShareMediaCategory(e.target.value)}>
+                                        <MenuItem value={'NONE'}>Juste Text</MenuItem>
+                                        <MenuItem value={'ARTICLE'}>Avec Article</MenuItem>
+                                        <MenuItem value={'IMAGE'}>Avec Image</MenuItem>
+                                </Select>
                             </Grid>
-                            {this.mediaRender()}
-                            <Button variant="contained" color="primary" onClick={()=>{this.onSendData()}}>
-                                Create
-                            </Button>
-                        </CardContent>
-                    </Card>
-                    {this.postRender()}
-                </Container>
-            </div>
-        )
-    }
-}
+                            <Grid item xs={12} md={2}>
+                                <Select
+                                    labelId="Visibilité"
+                                    id="visibility"
+                                    value={visibility}
+                                    onChange={e=>setVisibility(e.target.value)}>
+                                        <MenuItem value={'PUBLIC'}>Publique</MenuItem>
+                                        <MenuItem value={'CONNECTIONS'}>connexions</MenuItem>
+                                </Select>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <TextField 
+                                    fullWidth 
+                                    label="Contenu du post" 
+                                    id="shareCommentary"
+                                    value={shareCommentary}
+                                    onChange={e=>setShareCommentary(e.target.value)}
+                                    multiline
+                                />
+                            </Grid>
+                        </Grid>
+                        {mediaRender()}
+                        <Button variant="contained" color="primary" onClick={onSendData}>
+                            Create
+                        </Button>
+                    </CardContent>
+                </Card>
+                {postRender()}
+            </Container>
+        </div>
+    )
+};
 
 export default Menu;
