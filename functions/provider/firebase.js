1846f5e755b7d864da7b863545ed4a5d870ce9f9
Marc-Etienne Dartus <marcetienne.dartus@gmail.com>
2020-06-09 07:32:55 +0200

    Update admin credential

--- functions/provider/firebase.js
+++ functions/provider/firebase.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
 
-const pathServiceAccount = process.env.GCLOUD_PROJECT === "dev-feedmyflow" ? '../service-account-dev.json' : '../service-account-prod.json';
-const serviceAccount = require(pathServiceAccount);
+// const pathServiceAccount = process.env.GCLOUD_PROJECT === "dev-feedmyflow" ? '../service-account-dev.json' : '../service-account-prod.json';
+// const serviceAccount = require(pathServiceAccount);
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
+// });
+
+admin.initializeApp();
 
 module.exports = { admin };
